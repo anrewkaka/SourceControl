966e9154059050fc282f23251bd1edb612af0f36
anrewkaka <mr.anrew@gmail.com>
2015-10-08 23:30:25 +0700

    add Util

--- opencart/system/startup.php
+++ opencart/system/startup.php
@@ -56,7 +56,7 @@ function modification($filename) {
 	if (substr($filename, 0, strlen(DIR_SYSTEM)) == DIR_SYSTEM) {
 		$file = DIR_MODIFICATION . 'system/' . substr($filename, strlen(DIR_SYSTEM));
 	}
-	
+
 	if (is_file($file)) {
 		return $file;
 	}
@@ -67,12 +67,12 @@ function modification($filename) {
 // Autoloader
 function autoload($class) {
 	$file = DIR_SYSTEM . 'library/' . str_replace('\\', '/', strtolower($class)) . '.php';
-	
+
 	if (is_file($file)) {
 		include_once(modification($file));
 		return true;
 	}
-	
+
 	return false;
 }
 
@@ -91,3 +91,6 @@ require_once(modification(DIR_SYSTEM . 'engine/registry.php'));
 // Helper
 require_once(DIR_SYSTEM . 'helper/json.php');
 require_once(DIR_SYSTEM . 'helper/utf8.php');
+
+// Utilities
+require_once(DIR_SYSTEM . 'util/StringUtil.php');
